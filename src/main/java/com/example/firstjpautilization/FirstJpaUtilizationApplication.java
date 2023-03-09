eae31fdda6fdad322c0778101bf9ecc147b3de88
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-03-09 21:38:39 +0900

    Initial commit

---
+++ src/main/java/com/example/firstjpautilization/FirstJpaUtilizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstjpautilization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJpaUtilizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJpaUtilizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstjpautilization/FirstJpaUtilizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstjpautilization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJpaUtilizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
